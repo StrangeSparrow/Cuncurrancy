70ac0a564bb632ca4d5cf09493d774ffad13e5fe
StrangeSparrow <strangesparrows@gmail.com>
2021-12-31 00:38:45 +0300

    'init'

---
+++ src/main/java/org/egor/code/shild/NewThread.java
@@ -0,0 +1,22 @@
+package org.egor.code.shild;
+
+public class NewThread extends Thread {
+    public NewThread() {
+        super("Demo");
+        System.out.println("Дочерний поток создан: " + this);
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Дочерний поток: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Дочерний поток прерван");
+        }
+        System.out.println("Дочерний поток завершен");
+    }
+}
